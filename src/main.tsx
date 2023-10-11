aef83ab7ffdeec3ddb8c123af92d77f2e160a3d5
puneet khandal <lietester.00@gmail.com>
2023-10-11 11:48:28 +0530

    folder file changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./input.css";
-import Login from "./Components/LoginComponent";
-import Signup from "./Components/RegisterComponent";
-import Home from "./Components/HomeComponent";
+import Login from "./pages/LoginComponent";
+import Signup from "./pages/RegisterComponent";
+import Home from "./pages/HomeComponent";
 import { AuthProvider } from "./context/AuthProvider";
-import VerifyComponent from "./Components/VerifyComponent";
+import VerifyComponent from "./components/VerifyComponent";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
