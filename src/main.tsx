c5ebf2fcf6ead508b2f84d726664c333ed9cfca1
puneet khandal <lietester.00@gmail.com>
2023-10-11 12:51:25 +0530

    src revert

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./input.css";
-
+import Login from "./pages/LoginComponent";
+import Signup from "./pages/RegisterComponent";
+import Home from "./pages/HomeComponent";
 import { AuthProvider } from "./context/AuthProvider";
-
-import App from "./App";
+import VerifyComponent from "./components/VerifyComponent";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
       <BrowserRouter>
          <AuthProvider>
             <Routes>
-               <Route path="/*" element={<App />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/register" element={<Signup />} />
+               <Route path="/" element={<VerifyComponent />}>
+                  <Route path="/" element={<Home />} />
+               </Route>
             </Routes>
          </AuthProvider>
       </BrowserRouter>
